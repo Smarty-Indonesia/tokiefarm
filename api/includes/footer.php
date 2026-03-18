361c33418457cc54da64bd90ad0f8f0d729cf381
Smarty Indonesia <smartyindo@gmail.com>
2026-03-18 12:13:17 +0700

    update year

--- api/includes/footer.php
+++ api/includes/footer.php
@@ -24,7 +24,7 @@
                             <a class="logo-light" href="index.php"><img src="../assets/images/logo/logo.png" alt="logo"></a>
                             <a class="logo-dark" href="index.php"><img src="../assets/images/logo/logo2.png" alt="logo"></a>
                         </div>
-                        <p class="description mt--30"><span>© <script>new Date().getFullYear()>2012&&document.write(new Date().getFullYear());</script> - <a href="https://spy-token.io" target="_blank">SPY Token</a> - All rights reserved - <a href="terms-condition.php">Terms</a> - <a href="privacy-policy.php">Privacy Policy</a></p>
+                        <p class="description mt--30"><span>© 2020-2024 - <a href="https://spy-token.io" target="_blank">SPY Token</a> - All rights reserved - <a href="terms-condition.php">Terms</a> - <a href="privacy-policy.php">Privacy Policy</a></p>
                         
                         <a href="https://t.me/Tokie_Ind" target="_blank">
                             <img src="../assets/images/icons/telegram.png" width="35">
